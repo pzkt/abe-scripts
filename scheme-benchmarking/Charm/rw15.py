02f7f95f62af768a8f20fa603a52c0bfb298c47a
RustingRobot <RustingRobot@protonmail.com>
2025-05-14 22:42:55 +0200

    rw15 full benchmark

--- scheme-benchmarking/Charm/rw15.py
+++ scheme-benchmarking/Charm/rw15.py
@@ -1,5 +1,5 @@
 from charm.toolbox.pairinggroup import PairingGroup,GT
-from charm.schemes.abenc.abenc_maabe_rw15 import MaabeRW15
+from charm.schemes.abenc.abenc_maabe_rw15 import MaabeRW15,merge_dicts
 
 import timeit
 
@@ -32,7 +32,10 @@ def main():
         cipher_text = maabe.encrypt(public_parameters, public_keys, msg, access_policy)
 
     def decrypt():
-        decrypted_msg = maabe.decrypt(public_parameters, user_keys, cipher_text)
+        decrypted_msg = maabe.decrypt(public_parameters, combined_keys, cipher_text)
+
+    def keymerge():
+        merged_dicts = merge_dicts(keys1, keys2)
 
     print("setup benchmark")
     timer = (timeit.timeit(setup = "gc.enable()", stmt = setup, number = repeats))/repeats
@@ -74,19 +77,98 @@ def main():
         public_keys = {'AUTH0': public_key}
         cipher_text = maabe.encrypt(public_parameters, public_keys, msg, access_policy)
         user_keys = maabe.multiple_attributes_keygen(public_parameters, secret_key, gid, attr)
-        
+        combined_keys = {'GID': gid, 'keys': user_keys}
         timer = (timeit.timeit(setup = "gc.enable()", stmt = decrypt, number = repeats))/repeats
         note("AND decrypt", a, timer)
 
     print("OR decrypt benchmark")
     for a in attribute_counts:
         access_policy = f"({' or '.join(f'ATTRIBUTE{i}@AUTH0' for i in range(a))})"
-        attr = [f"ATTRIBUTE{a-1}"]
-        cipher_text = cpabe.encrypt(master_public_key, msg, access_policy)
-        secret_key = cpabe.keygen(master_public_key, master_key, attr)
-
+        attr = [f"ATTRIBUTE{a-1}@AUTH0"]
+        (public_key, secret_key) = maabe.authsetup(public_parameters, "AUTH0")
+        public_keys = {'AUTH0': public_key}
+        cipher_text = maabe.encrypt(public_parameters, public_keys, msg, access_policy)
+        user_keys = maabe.multiple_attributes_keygen(public_parameters, secret_key, gid, attr)
+        combined_keys = {'GID': gid, 'keys': user_keys}
         timer = (timeit.timeit(setup = "gc.enable()", stmt = decrypt, number = repeats))/repeats
         note("OR decrypt", a, timer)
+
+    print("Key Merging")
+    for a in attribute_counts:
+        attr0 = [f"ATTRIBUTE{i}@AUTH0" for i in range(a)]
+        attr1 = [f"ATTRIBUTE{i}@AUTH1" for i in range(a)]
+
+        (public_key0, secret_key0) = maabe.authsetup(public_parameters, "AUTH0")
+        (public_key1, secret_key1) = maabe.authsetup(public_parameters, "AUTH1")
+
+        keys1 = maabe.multiple_attributes_keygen(public_parameters, secret_key0, gid, attr0)
+        keys2 = maabe.multiple_attributes_keygen(public_parameters, secret_key1, gid, attr1)
+
+        timer = (timeit.timeit(setup = "gc.enable()", stmt = keymerge, number = repeats))/repeats
+        note("Key Merging", f"{a} x {a}", timer)
+
+    print("Complex AND encrypt benchmark")
+    for a in attribute_counts:
+        public_key_array = [None] * a
+        secret_key_array = [None] * a
+        for i in range(a):
+            (public_key_array[i], secret_key_array[i]) = maabe.authsetup(public_parameters, f"AUTH{i}")
+
+        public_keys = {f"AUTH{i}": public_key_array[i] for i in range(a)}
+        access_policy = f"({' and '.join(f'ATTRIBUTE0@AUTH{i}' for i in range(a))})"
+        timer = (timeit.timeit(setup = f"gc.enable()", stmt = encrypt, number = repeats))/repeats
+        note("Complex AND encrypt", a, timer)
+
+    print("Complex OR encrypt benchmark")
+    for a in attribute_counts:
+        public_key_array = [None] * a
+        secret_key_array = [None] * a
+        for i in range(a):
+            (public_key_array[i], secret_key_array[i]) = maabe.authsetup(public_parameters, f"AUTH{i}")
+
+        public_keys = {f"AUTH{i}": public_key_array[i] for i in range(a)}
+        access_policy = f"({' or '.join(f'ATTRIBUTE0@AUTH{i}' for i in range(a))})"
+        timer = (timeit.timeit(setup = f"gc.enable()", stmt = encrypt, number = repeats))/repeats
+        note("Complex OR encrypt", a, timer)
+
+    print("Complex AND decrypt benchmark")
+    for a in attribute_counts:
+        public_key_array = [None] * a
+        secret_key_array = [None] * a
+        user_keys = []
+        for i in range(a):
+            (public_key_array[i], secret_key_array[i]) = maabe.authsetup(public_parameters, f"AUTH{i}")
+            user_keys = merge_dicts(user_keys, maabe.multiple_attributes_keygen(public_parameters, secret_key_array[i], gid, [f"ATTRIBUTE0@AUTH{i}"]))
+
+        public_keys = {f"AUTH{i}": public_key_array[i] for i in range(a)}
+        access_policy = f"({' and '.join(f'ATTRIBUTE0@AUTH{i}' for i in range(a))})"
+
+        cipher_text = maabe.encrypt(public_parameters, public_keys, msg, access_policy)
+
+        combined_keys = {'GID': gid, 'keys': user_keys}
+
+        timer = (timeit.timeit(setup = f"gc.enable()", stmt = decrypt, number = repeats))/repeats
+        note("Complex AND decrypt", a, timer)
+
+    print("Complex OR decrypt benchmark")
+    for a in attribute_counts:
+        public_key_array = [None] * a
+        secret_key_array = [None] * a
+
+        for i in range(a):
+            (public_key_array[i], secret_key_array[i]) = maabe.authsetup(public_parameters, f"AUTH{i}")
+
+        user_keys = maabe.multiple_attributes_keygen(public_parameters, secret_key_array[a-1], gid, [f"ATTRIBUTE0@AUTH{a-1}"])
+
+        public_keys = {f"AUTH{i}": public_key_array[i] for i in range(a)}
+        access_policy = f"({' or '.join(f'ATTRIBUTE0@AUTH{i}' for i in range(a))})"
+
+        cipher_text = maabe.encrypt(public_parameters, public_keys, msg, access_policy)
+
+        combined_keys = {'GID': gid, 'keys': user_keys}
+
+        timer = (timeit.timeit(setup = f"gc.enable()", stmt = decrypt, number = repeats))/repeats
+        note("Complex OR decrypt", a, timer)
 
 if __name__ == "__main__":
     debug = True
