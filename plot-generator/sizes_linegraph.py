be3fba55107acf1083c949ae43daa6393bb3baac
RustingRobot <RustingRobot@protonmail.com>
2025-06-02 22:33:38 +0200

    openabe ct size test results

--- plot-generator/sizes_linegraph.py
+++ plot-generator/sizes_linegraph.py
@@ -4,7 +4,7 @@ import numpy as np
 import random
 
 path = "../scheme-benchmarking/Results/storage/"
-files = ["charm_bsw07_ct","charm_fame_ct","charm_rw15_ct","charm_waters11_ct","charm_yahk14_ct", "circl_tkn20_ct","gofe_fame_ct","rabe_bsw07_ct","rabe_fame_ct","rabe_ghw11_ct"]
+files = ["charm_bsw07_ct","charm_fame_ct","charm_rw15_ct","charm_waters11_ct","charm_yahk14_ct", "circl_tkn20_ct","gofe_fame_ct","openabe_waters11_ct","rabe_bsw07_ct","rabe_fame_ct","rabe_ghw11_ct"]
 markers = ["o","s","D","P","^","o","s","D","P","o","o","o","o","s","D"];
 lib_colors = ["#003a7d"] * 5 + ["#008dff", "#d83034" , "#c701ff"] + ["#4ecb8d"] * 3
 
@@ -12,7 +12,7 @@ data = []
 for file in files:
     try:
         csv_file = pd.read_csv(path + file + ".csv")
-        selectRow = csv_file.iloc[0]
+        selectRow = csv_file.iloc[10]
         selectRow = selectRow.drop(["index", "attributes"], errors="ignore")
         if (file not in ["rabe_bsw07_ct", "rabe_fame_ct", "rabe_ghw11_ct"]):
             selectRow = selectRow[~selectRow.index.str.startswith("single")]
@@ -29,14 +29,14 @@ x_positions = np.arange(len(data[0]))
 reference_line = 2 ** x_positions
 
 # Plot the reference line
-plt.plot(data[0].index, reference_line, 'r--', label=f'Exponential growth (factor 2)')
+#plt.semilogy(data[0].index, reference_line, 'r--', label=f'Exponential growth (factor 2)')
 
 for i in range(len(data)):
     if (i in []): #cull certain entries
         continue
-    #adjusted_values = data[i].values - reference_line
-    #adjusted_values = np.where(adjusted_values <= 0, 1e-6, adjusted_values)
-    plt.semilogy(data[i].index, data[i].values, label=files[i] , marker=markers[i], linestyle="-", color=lib_colors[i])
+    adjusted_values = data[i].values - reference_line
+    adjusted_values = np.where(adjusted_values <= 0, 1e-6, adjusted_values)
+    plt.semilogy(data[i].index, adjusted_values, label=files[i] , marker=markers[i], linestyle="-", color=lib_colors[i])
 
 plt.legend(fontsize=10)
 plt.grid(True, linestyle='--', alpha=0.7)
